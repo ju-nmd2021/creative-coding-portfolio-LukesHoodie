0ad75cc6a4d7b7e46d153da7397a88391ef78e64
LukesHoodie <klla20zl@student.ju.se>
2023-08-31 11:36:37 +0200

    finished Wave and tried making a tree

---
+++ experiments/wave.js
@@ -0,0 +1,36 @@
+//inspired by Colorful Coding: Simple Sine wave animation (https://www.youtube.com/watch?v=ktPnruyC6cc)
+//but scaled and adapted to a smaller canvas
+
+function setup() {
+    createCanvas(innerWidth, innerHeight);
+    frameRate(30);
+    background(20, 20, 20);
+
+    angleMode(DEGREES);
+    rectMode(CENTER);
+  }
+
+function draw() {
+    background(20, 20, 20);
+
+    push();
+    noFill();
+
+    translate(width /2, height /2.5);
+
+    for (var i = 0; i < 100; i++) {
+    push();
+     rotate(sin(frameCount + i) *100);
+
+      var r = map(sin(frameCount), -1, 1, 50, 255);
+      var g = map(cos(frameCount/2), -1, 1, 50, 255);
+      var b = map(sin(frameCount/4), -1, 1, 50, 255);
+
+      stroke(r, g, b);
+
+     rect(0, 0, 300 - i * 3, 300 - i * 3, 100 - i);
+    pop();
+    }
+
+    pop();
+}
